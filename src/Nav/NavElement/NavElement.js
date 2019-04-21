75b0a45338261e85155d8e8fb3ac96b0c5d5759e
davidpitoniak <33641901+davidpitoniak@users.noreply.github.com>
2019-04-21 19:25:19 -0400

    Fixing icon alignment on mobile

--- src/Nav/NavElement/NavElement.js
+++ src/Nav/NavElement/NavElement.js
@@ -6,7 +6,7 @@ const navElement = (props) => {
     return(
       <div className='nav-element'>
         <a className="link hvr-skew-forward" href={props.link}>
-            <FontAwesomeIcon icon={[props.type, props.icon]} />
+            <FontAwesomeIcon icon={[props.type, props.icon]} className="icon"/>
         </a>
       </div>  
     );
