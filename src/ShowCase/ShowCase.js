5ef1c7212b0a68f840e9215b5de21f6758897df9
David Pitoniak <davidpitoniak@outlook.com>
2019-04-21 10:34:51 -0400

    Added new ShowCaseElement.

--- src/ShowCase/ShowCase.js
+++ src/ShowCase/ShowCase.js
@@ -5,7 +5,8 @@ import './ShowCase.css'
 const showCase = (props) => {
     return (
         <div className="show-case">
-            <ShowCaseElement image={require('./ShowCaseElement/download.jpg')} alt="" link='https://github.com/Cryoexn/PersonalWebsite' title='Personal Website' author='David Pitoniak' desc='This is my personal portfolio website that im currently developing to display my coding projects aswell as learn new technologies.'/>
+            <ShowCaseElement image={require('../images/download.jpg')} alt="" link='https://github.com/Cryoexn/PersonalWebsite' title='Personal Website' author='David Pitoniak' desc='This is my old static personal portfolio website.'/>
+            <ShowCaseElement image={require('../images/download.jpg')} alt="" link='https://github.com/Cryoexn/ReactPortfolioSite' title='React Personal Site (current)' author='David Pitoniak' desc='This is my personal portfolio website that im currently developing to display my coding projects aswell as learn new technologies.'/>
         </div>
     );
 }
