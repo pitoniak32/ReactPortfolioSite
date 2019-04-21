50576a49f03f07fec07176dde6e48a0eeda65b63
David Pitoniak <davidpitoniak@outlook.com>
2019-04-21 10:26:36 -0400

    Added more style to NavElements.

--- src/Nav/NavElement/NavElement.js
+++ src/Nav/NavElement/NavElement.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const navElement = (props) => {
     return(
       <div className='nav-element'>
-        <a className="link" href={props.link}>
+        <a className="link hvr-skew-forward" href={props.link}>
             <FontAwesomeIcon icon={[props.type, props.icon]} />
         </a>
       </div>  
