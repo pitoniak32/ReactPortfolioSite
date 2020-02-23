0d0593b2a95baea241fcbddd94d78c51d703df00
dhp6397 <dhp6397@rit.edu>
2020-02-22 19:21:06 -0500

    Fixed mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Nav from './Nav/Nav';
-import ShowCase from './ShowCase/ShowCase';
+//import ShowCase from './ShowCase/ShowCase';
 import WebsiteTitle from './WebsiteTitle/WebsiteTitle';
 import Blurb from './Blurb/Blurb';
 import './App.css';
@@ -16,14 +16,14 @@ library.add(fab, faGithub, faLinkedin, faHandshake);
 var blurbContent = "I am currently a Computer Science student at Rochester Institute of Technology. " +
                    "I enjoy writing code and learning new technologies to expand my knowledge in software. " +
                    "I have also taken an interest in Arch Linux and i3 window manager and have been experimenting " +
-                   "with different Distributions. I have written a few personal projects for practice including this " + 
+                   "with different distributions. I have written a few personal projects for practice including this " + 
                    "website. I am always prepared and excited to learn new things.";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WebsiteTitle title='{ Cryoexn.Club } David Pitoniak' />
+        <WebsiteTitle title='Cryoexn.Club David Pitoniak' />
         <Blurb contentText={blurbContent} />
         <Nav />
       </div>
